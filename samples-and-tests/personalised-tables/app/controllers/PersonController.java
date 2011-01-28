9789ce1957989fb0b89dfd7dc24fae6c6b75965a
Steve Chaloner <github@objectify.be>
2011-01-28 09:58:15 +0100

    added example of using a server-side table model with ajax

--- samples-and-tests/personalised-tables/app/controllers/PersonController.java
+++ samples-and-tests/personalised-tables/app/controllers/PersonController.java
@@ -1,15 +1,18 @@
 package controllers;
 
 import controllers.tabularasa.TableController;
+import models.ExampleUser;
+import models.PersonalisedTablesPerson;
 import models.tabularasa.TableModel;
-import play.mvc.*;
 
-import java.util.*;
-
-import models.*;
+import play.mvc.Controller;
+import play.mvc.Util;
 import utils.LedPropertyParser;
 
-public class PersonController extends Controller {
+import java.util.List;
+
+public class PersonController extends Controller
+{
 
     private static final String[] STEVE_COLUMNS = {"name", "location"};
     private static final String[] GREET_COLUMNS = {"location", "name"};
@@ -33,6 +36,34 @@ public class PersonController extends Controller {
 
         render(tableModel,
                people);
+    }
+
+    public static void ajaxIndex(String userName)
+    {
+        // in a real application, userName would come from the current user
+        userName = (userName == null) ? "steve" : userName;
+        ExampleUser user = ExampleUser.findByUserName(userName);
+        TableModel tableModel = TableController.getTableModel(user.tableOwner,
+                                                              VIEW_ID,
+                                                              TABLE_ID,
+                                                              "steve".equals(userName) ? STEVE_COLUMNS : GREET_COLUMNS);
+
+        render(tableModel);
+    }
+
+    public static void data(String tableId,
+                            Integer iDisplayStart,
+                            Integer iDisplayLength,
+                            String sColumns,
+                            String sEcho)
+    {
+        List<PersonalisedTablesPerson> people = PersonalisedTablesPerson.all().from(iDisplayStart == null ? 0 : iDisplayStart).fetch(iDisplayLength == null ? 10 : iDisplayLength);
+        long totalRecords = PersonalisedTablesPerson.count();
+        TableController.renderJSON(people,
+                                   PersonalisedTablesPerson.class,
+                                   totalRecords,
+                                   sColumns,
+                                   sEcho);
     }
 
     @Util
